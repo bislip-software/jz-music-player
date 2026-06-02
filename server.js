01fcf2d9c93d1582ae4de02de73cc119340418b9
Javier Zabala <javier_bislip@hotmail.com>
2026-06-01 22:07:16 -0300

    Remove env check when serving static assets

--- server.js
+++ server.js
@@ -18,15 +18,12 @@ app.use(express.json({ extended: false }));
 //Define Routes
 app.use("/api", require("./routes/index"));
 
-//Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-  // Always renders index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// Set static folder
+app.use(express.static("client/build"));
+// Always renders index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5001;
 
